32e865c9f4c275ef060885a5ee0a3ebf1d9c5b7e
Piotr Praszkiewicz <piotrek1009@gmail.com>
2020-04-07 17:16:21 +0200

    added registration v1

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Regform from "./app/components/Regform";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Regform />
     </View>
   );
 }
@@ -12,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#121212",
+    paddingLeft: 60,
+    paddingRight: 60
   },
 });
